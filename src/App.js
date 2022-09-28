145c0813775b2bc9e043aa24297e71e5e48826bf
PhungThinhVuong <phungthinhvuong2101@gmail.com>
2022-09-28 20:43:25 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import BaiTapGioHang from './Props/GioHang/BaiTapGioHang';
+import GioHang from './Props/GioHang/GioHang';
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <BaiTapGioHang/>
+    </div>
+  );
+}
+
